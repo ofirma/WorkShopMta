ea9259c34c99b237bdc129a1ca87283dd55e1e96
ofirma <ofirmalca@gmail.com>
2014-07-15 18:38:11 +0300

    Commit VidoesByFilter and VideosOfUser

---
+++ PicPoint/PicPoint/Controllers/GetVideosByFilterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using PicPoint.Models;
+using System.Net;
+using System.Configuration;
+using System.Web.Http;
+
+namespace PicPoint.Controllers
+{
+    public class GetVideosByFilterController : ApiController
+    {
+        public HttpResponseMessage GetVideosOfUser(string filter)
+        {
+            Database1Entities1 de = new Database1Entities1();
+            List<Trips> trips = new List<Trips>();
+
+            foreach (var obj in de.Trips)
+            {
+                if (obj.trip_id.Contains(filter))
+                {
+                    trips.Add(obj);
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, trips, Configuration.Formatters.JsonFormatter);
+        }
+
+    }
+}
---
+++ PicPoint/PicPoint/Controllers/GetVideosOfUserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+using PicPoint.Models;
+using System.Net;
+
+namespace PicPoint.Controllers
+{
+    public class GetVideosOfUserController : ApiController
+    {
+        public HttpResponseMessage GetVideosOfUser(string user)
+    {
+        Database1Entities1 de = new Database1Entities1();
+        List<Trips> trips = new List<Trips>();
+
+        foreach (var obj in de.Trips)
+        {
+            if (obj.username == user)
+            {
+                trips.Add(obj);
+            }
+        }
+
+        return Request.CreateResponse(HttpStatusCode.OK, trips, Configuration.Formatters.JsonFormatter);
+    }
+
+    }
+}
