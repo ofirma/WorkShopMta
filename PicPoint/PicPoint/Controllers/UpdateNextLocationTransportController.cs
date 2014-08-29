2ef5c35c133da7dd6a7d913801b6bac92bbb0ce5
ofirma <ofirmalca@gmail.com>
2014-08-29 15:10:38 +0300

    no message

--- PicPoint/PicPoint/Controllers/UpdateNextLocationTransportController.cs
+++ PicPoint/PicPoint/Controllers/UpdateNextLocationTransportController.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PicPoint.Models;
+using Newtonsoft.Json.Linq;
+using System.Web.Http;
 
 namespace PicPoint.Controllers
 {
-    public class UpdateNextLocationTransportController : Controller
+    public class UpdateNextLocationTransportController : ApiController
     {
         //
         // GET: /UpdateNextLocationTransport/
 
-        public bool Get(string locId, string nextTransport)
+        [System.Web.Mvc.HttpPost]
+        public bool Post(object json)
         {
-            List<Locations> list = DBEntities.Proxy.Locations.Where(x => x.location_id == locId).ToList();
+            JObject obj = json as JObject;
+            string id = obj["locationId"].ToString();
+            string transportType = obj["transportType"].ToString();
+            List<Locations> list = DBEntities.Proxy.Locations.Where(x => x.location_id == id).ToList();
             if (list != null && list.Count == 1 && list[0] != null)
             {
-                list[0].travelModeToNextLocation = nextTransport;
+                list[0].travelModeToNextLocation = transportType;
                 DBEntities.Proxy.SaveChanges();
                 return true;
             }
